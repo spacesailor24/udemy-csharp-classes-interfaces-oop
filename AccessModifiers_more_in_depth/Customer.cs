773660fcad022d91daff7294f2f2e24925118af4
Wyatt Barnes <wyattbarnes@protonmail.com>
2018-04-20 14:23:42 -0500

    Section 4 Lecture 21

---
+++ AccessModifiers_more_in_depth/Customer.cs
@@ -0,0 +1,29 @@
+﻿namespace AccessModifiers
+{
+    using System;
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var rating = this.CalculateRating(excludeOrder: true);
+
+            if (rating == 0)
+            {
+                Console.WriteLine("Promoted to level 1");
+            }
+            else
+            {
+                Console.WriteLine("Promoted to level 2");
+            }
+        }
+
+        private int CalculateRating(bool excludeOrder)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AccessModifiers_more_in_depth/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Promote();
+        }
+    }
+}
